b72c39a1eee9c3cf33264aeb873cb11cf0ad20cd
KebbaPhilip <kebbaphilip@gmail.com>
2026-01-21 09:27:38 +0000

    Day 13: Add debugging practice

---
+++ Day13/problem1.py
@@ -0,0 +1,8 @@
+from random import randint
+dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_images[dice_num])
+
+## solution
+dice_num = randint(0, 5)
+print(dice_num)
--- Day13/problem2.py
+++ Day13/problem2.py
@@ -1,5 +1,6 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+#problem
 # dice_num = randint(1, 6)
 # print(dice_images[dice_num])
 
